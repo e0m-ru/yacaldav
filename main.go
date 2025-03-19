6e7a441bed2f5f9b86df8b8330694b3259dbf86a
Aleksandr Babushkin <i@e0m.ru>
2025-03-19 16:44:01 +0300

    modified:   main.go 	deleted:    report/report.go

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package yacaldav
 import (
 	"context"
 	"fmt"
+	"io"
 	"time"
 
 	"github.com/e0m-ru/yacaldav/config"
 	"github.com/e0m-ru/yacaldav/logger"
-	"github.com/e0m-ru/yacaldav/report"
+	"github.com/emersion/go-ical"
 	"github.com/emersion/go-webdav/caldav"
 )
 
@@ -51,7 +52,46 @@ func MonthReport(client *caldav.Client, month time.Month) {
 		fmt.Fprintf(L.Logger.Writer(), ">>>>>>>>> %v\n  <<<<<<<<<<<<", calendar.Name)
 		claList, err := client.QueryCalendar(ctx, calendar.Path, BuildMonthRangeQuery(date))
 		L.Error(err)
-		report.PrintAllCalendarsData(L.Logger.Writer(), claList)
+		PrintAllCalendarsData(L.Logger.Writer(), claList)
 		fmt.Fprintf(L.Logger.Writer(), " =========================\n\n")
+	}
+}
+
+func printEvent(cal ical.Event) string {
+	startTime, _ := cal.DateTimeStart(time.Local)
+	endTime, _ := cal.DateTimeEnd(time.Local)
+	html := fmt.Sprintf("<p>start date : %s</p>\n<p>start time: %s</p>\n",
+		startTime.Format("2006-01-02"), startTime.Format("15:04"))
+	sy, sm, sd := startTime.Date()
+	ey, em, ed := endTime.Date()
+	if sy != ey || sm != em || sd != ed {
+		html += fmt.Sprintf("<p>end date: %s</p>", endTime.Format("2006-01-02"))
+	}
+	html += fmt.Sprintf("<p>end time: %s</p>\n", endTime.Format("15:04"))
+	title := getPropText(cal, ical.PropSummary)
+	desc := getPropText(cal, ical.PropDescription)
+	loc := getPropText(cal, ical.PropLocation)
+	// dep := getPropText(cal, "DEPARTMENT")
+	uid := getPropText(cal, ical.PropUID)
+	return html + fmt.Sprintf("uid: %s\n<h1>%s</h1>\n<p>Location: %s</p>\n<p>%s</p>\n", uid, title, loc, desc)
+}
+
+func getPropText(cal ical.Event, propName string) string {
+	prop := cal.Props.Get(propName)
+	if prop != nil {
+		text, _ := prop.Text()
+		return text
+	}
+	return ""
+}
+
+func PrintAllCalendarsData(w io.Writer, calendarList []caldav.CalendarObject) {
+	for _, c := range calendarList {
+		fmt.Fprintf(w, "path: %s\n", c.Path)
+		fmt.Fprintf(w, "modTime %s\n", c.ModTime)
+		fmt.Fprintf(w, "---------Events--------\n")
+		for _, e := range c.Data.Events() {
+			fmt.Fprintf(w, "%s\n-------------------------\n", printEvent(e))
+		}
 	}
 }
